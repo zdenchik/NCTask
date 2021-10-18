0eb6a8acbdf00f55add2da1b2c8c30576db994c2
Денис Зозуля <zdenchik@gmail.com>
2021-10-18 13:45:48 +0300

    pr3 v2

--- src/main/java/ua/edu/sumdu/j2se/zozulia/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/zozulia/tasks/ArrayTaskList.java
@@ -1,25 +1,65 @@
 package ua.edu.sumdu.j2se.zozulia.tasks;
 
-import java.util.ArrayList;
+import javax.smartcardio.TerminalFactory;
+import java.util.Arrays;
 
 /**
  * Class ArrayTaskList - main purpose is to store default or
  * a repeatable task`s
  *
- * @version 1.10 18 Oct 2021
+ * @version 1.20 18 Oct 2021
  * @author Denis Zozulia
  */
 public class ArrayTaskList {
 
-    private ArrayList<Task> ArrayList = new ArrayList<>();
+    private Task ArrayList[];
 
-    public void add(Task task){ this.ArrayList.add(task);}
+    public ArrayTaskList(){
+        ArrayList = new Task[0];
+    }
 
-    public boolean remove(Task task){ return  this.ArrayList.remove(task);}
+    public void add(Task task) {
+        Task TempArray[] = new Task[ArrayList.length + 1];
+        for (int i = 0; i < ArrayList.length; i++) {
 
-    public int size(){ return this.ArrayList.size();}
+            TempArray[i] = ArrayList[i];
 
-    public Task getTask(int index){ return this.ArrayList.get(index);}
+        }
+        TempArray[TempArray.length - 1] = task;
+        this.ArrayList = TempArray.clone();
+    }
+
+    public boolean remove(Task task){
+
+        boolean Answer = false;
+        int k = 0;
+        for(int i = 0; i < ArrayList.length;i++){
+            if(ArrayList[i] == task){
+                Answer = true;
+                k = i;
+                break;
+            }
+        }
+
+        if(Answer){
+            Task TempArray[] = new Task[ArrayList.length-1];
+            for(int i = 0, j = 0; i < ArrayList.length;i++){
+                if(i != k){
+
+                    TempArray[j++] = ArrayList[i];
+
+                }
+            }
+            
+            this.ArrayList = TempArray.clone();
+        }
+
+        return Answer;
+    }
+
+    public int size(){ return this.ArrayList.length;}
+
+    public Task getTask(int index){ return this.ArrayList[index];}
 
     public ArrayTaskList incoming(int from, int to){
 
