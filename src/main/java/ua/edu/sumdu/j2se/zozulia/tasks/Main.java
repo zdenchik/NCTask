b64f1b7d066bc38d3dafb411d70650ac35d2520a
Денис Зозуля <zdenchik@gmail.com>
2021-10-19 11:00:17 +0300

    rework v1

--- src/main/java/ua/edu/sumdu/j2se/zozulia/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/zozulia/tasks/Main.java
@@ -1,7 +1,11 @@
 package ua.edu.sumdu.j2se.zozulia.tasks;
 
+import java.time.temporal.TemporalQueries;
+
 public class Main {
 
-	public static void main(String[] args) { System.out.println("Hello"); }
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
 
